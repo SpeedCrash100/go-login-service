54665d4a4560c0e927aa426f99e020d399601960
SpeedCrash100 <SpeedCrash100@yandex.ru>
2023-06-28 05:42:43 +0300

    Set log level depending on config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/SpeedCrash100/go-login-service/pkg/consts"
 	"github.com/gin-contrib/logger"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
@@ -14,6 +16,7 @@ import (
 func initConfig() {
 	viper.SetDefault(consts.CONFIG_PORT, 8080)
 	viper.SetDefault(consts.CONFIG_IP, "0.0.0.0")
+	viper.SetDefault(consts.CONFIG_IS_DEBUG, true)
 
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
@@ -36,14 +39,26 @@ func initRoutes(r *gin.Engine) {
 }
 
 func main() {
+	// Start-up log configuration
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+
+	// Configs
 	initConfig()
 
+	// Set Logging level
+	if viper.GetBool(consts.CONFIG_IS_DEBUG) {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	// Prepare engine
 	e := gin.New()
 	e.Use(logger.SetLogger())
 	e.Use(gin.Recovery())
 
 	initRoutes(e)
 
+	// Start
 	address := viper.GetString(consts.CONFIG_IP) + ":" + viper.GetString(consts.CONFIG_PORT)
 	e.Run(address)
 
--- pkg/consts/config.go
+++ pkg/consts/config.go
@@ -1,6 +1,7 @@
 package consts
 
 const (
-	CONFIG_IP   = "ip"
-	CONFIG_PORT = "port"
+	CONFIG_IP       = "ip"
+	CONFIG_PORT     = "port"
+	CONFIG_IS_DEBUG = "is_debug"
 )
